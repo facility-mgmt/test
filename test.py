0546c3f194cdbbe5e2a7dd610fe64de48554a1ca
facility-mgmt <45282057+facility-mgmt@users.noreply.github.com>
2018-12-04 08:27:54 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,20 @@
-import urllib.request
+import requests
 
-#urllist=["http://133.34.148.2"]
+#urllist = ["http://133.34.148.2"]
 urllist=["http://133.34.148.%d"%i for i in range(2,99)]
 
 for url in urllist:
 
-    req = urllib.request.Request(url)
     try:
-        with urllib.request.urlopen(req) as res:
-            body = res.read()
-        #print(body)
-    except urllib.error.HTTPError as err:
-        print(err.code)
-    except urllib.error.URLError as err:
-        print(err.reason)
+        #body = requests.get(url)
+        body = requests.post(url + "/cgi/csdcgi", {
+            'strId': 'admin',
+            'strPwd': 'ynu-mgmt'
+        })
+        print url, " OK"
+        #
+        #TODO: Get status from status page
+        #
+    except err:
+        print url, " ERROR"
+        print err.code
