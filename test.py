d9eaf3719b9bc8a25cbe03e5865c9dbcde8d24b3
facility-mgmt <45282057+facility-mgmt@users.noreply.github.com>
2018-12-19 08:54:03 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import requests
+import re
 
 #urllist = ["http://133.34.148.2"]
 urllist=["http://133.34.148.%d"%i for i in range(2,99)]
@@ -8,13 +9,14 @@ for url in urllist:
     try:
         #body = requests.get(url)
         body = requests.post(url + "/cgi/csdcgi", {
-            'strId': 'admin',
-            'strPwd': 'ynu-mgmt'
+            'strId': 'admin', # ID
+            'strPwd': 'ynu-mgmt' #PASSWORD
         })
         print url, " OK"
         #
         #TODO: Get status from status page
         #
+        #result=re.match("<body>",body)
     except err:
         print url, " ERROR"
         print err.code
